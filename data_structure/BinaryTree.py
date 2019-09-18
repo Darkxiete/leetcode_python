d7c8109f0536f852c561df896e1236aa57bfab0f
Darkxiete <470058433@qq.com>
2019-09-18 23:52:24 +0800

    Update Binary Tree 总结

--- data_structure/BinaryTree.py
+++ data_structure/BinaryTree.py
@@ -130,12 +130,14 @@ def show_btree_dfs_iterative4(root):
     :param root:
     :return:
     """
+    if not root:
+        return
     stack = []
     while True:
-        if root:
+        while root:
             stack.append(root)
             root = root.left
-        elif stack:
+        if stack:
             root = stack.pop()
             print(root.val)
             root = root.right
@@ -164,15 +166,41 @@ def show_btree_dfs_iterative5(root):
     print(ans[::-1])
 
 
+def post_order_traversal_iterative(root: BinaryTree):
+    if not root:
+        return
+    stack = []
+    last = None
+    while root or stack:
+        if root:
+            stack.append(root)
+            root = root.left
+        else:
+            node = stack[-1]
+            if node.right and last != node.right:
+                root = node.right
+            else:
+                print(node.val)
+                last = node
+                stack.pop()
+
+
 def show_btree_bfs_recursive1(root):
     pass
 
 
 def show_btree_bfs_iterative1(root):
+    """
+    level order
+    :param root:
+    :return:
+    """
+    if not root:
+        return
     queue = [root]
     while queue:
         root = queue[0]
-        queue = queue[1:]
+        queue = queue[1:]  # 模仿deque操作
         print(root.val)
         if root.left:
             queue.append(root.left)
@@ -180,9 +208,91 @@ def show_btree_bfs_iterative1(root):
             queue.append(root.right)
 
 
+def prev_order_traversal_morris(root: BinaryTree):
+    if not root:
+        return
+    curr = root
+    while curr:
+        prev = curr.left
+        if prev:
+            while prev.right and prev.right != curr:
+                prev = prev.right
+            if not prev.right:
+                prev.right = curr
+                print(curr.val)
+                curr = curr.left
+            else:
+                prev.right = None
+                curr = curr.right
+        else:
+            print(curr.val)
+            curr = curr.right
+
+
+def in_order_traversal_morris(root: BinaryTree):
+    if not root:
+        return
+    curr = root
+    while curr:
+        prev = curr.left
+        if prev:
+            while prev.right and prev.right != curr:
+                prev = prev.right
+            if not prev.right:
+                prev.right = curr
+                curr = curr.left
+            else:
+                prev.right = None
+                print(curr.val)  # 和pre-order的唯一不同点
+                curr = curr.right
+        else:
+            print(curr.val)
+            curr = curr.right
+
+
+def post_order_traversal_morris(root: BinaryTree):
+    def reverseNodes(start: BinaryTree, end: BinaryTree):
+        if start == end:
+            return
+        x, y, z = start, start.right, None
+        while x != end:
+            z = y.right
+            y.right = x
+            x = y
+            y = z
+
+    def reverseDealNodes(start, end):
+        reverseNodes(start, end)
+        node = end
+        while True:
+            print(node.val)
+            if node == start:
+                break
+            node = node.right
+        reverseNodes(end, start)
+
+    dummy = BinaryTree(0)
+    dummy.left = root
+    curr = dummy
+    while curr:
+        if curr.left:
+            prev = curr.left
+            while prev.right and prev.right != curr:
+                prev = prev.right
+            if not prev.right:
+                prev.right = curr
+                curr = curr.left
+            else:
+                reverseDealNodes(curr.left, prev)
+                prev.right = None
+                curr = curr.right
+        else:
+            curr = curr.right
+
+
 if __name__ == '__main__':
     nums1 = [1, 2, 3, 4, 5, 6]
     nums2 = [1, None, 2, 3]
     nums3 = ["b", "a", "f", None, None, "d", "g", None, None, None, None, "c", "e"]
     bt = create_btree(nums1)
-    show_btree_dfs_iterative5(bt)
+    post_order_traversal_morris(bt)
